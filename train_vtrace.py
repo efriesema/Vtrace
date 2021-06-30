eb588efd6d9b76693849be8bb039ac481ed46cd6
Ed Friesema <roninfilms01@gmail.com>
2021-06-30 09:09:15 -0700

    adding files

---
+++ train_vtrace.py
@@ -0,0 +1,155 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jun 21 10:20:40 2021
+
+@author: efriesema
+"""
+
+import numpy as np
+from collections import deque
+import torch
+
+from vtrace import VtraceAgent
+from utils import collect_trajectories, random_sample, make_mb_discounts
+from parallelEnv import parallelEnv
+
+def train(episode,env_name):
+    gamma = .99
+    best_score = -np.inf
+    goal_score = 195.0
+
+    nenvs = 8
+    rollout_length = 200
+    minibatches = 10*8
+    # Calculate the batch_size
+    nbatch = nenvs * rollout_length
+    optimization_epochs = 4
+    
+    device=torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+    #initializing agent
+    envs = parallelEnv(env_name, nenvs, seed=1234)
+    #Initializing Vtrace agent
+    agent = VtraceAgent(state_size=envs.observation_space.shape[0],
+                     action_size=envs.action_space.n, 
+                     seed=0,
+                     hidden_layers=[64,64],
+                     lr_policy=1e-4, 
+                     use_reset=True,
+                     device=device)
+    print("------------------")
+    print(agent.policy)
+    print("------------------")
+
+    # keep track of progress
+    mean_rewards = []
+    scores_window = deque(maxlen=100)
+    loss_storage = []
+
+    for i_episode in range(episode+1):
+        log_probs_old, states, actions, rewards, values, dones, vals_last = collect_trajectories(envs, agent.policy, rollout_length)
+
+        returns = np.zeros_like(rewards)
+        mb_discounts = make_mb_discounts(returns.shape,gamma)     
+        # convert to pytorch tensors and move to gpu if available
+        returns = torch.from_numpy(returns).float().to(device).view(-1,)
+
+        
+        for _ in range(optimization_epochs):
+            sampler = random_sample(nbatch, minibatches)
+            for inds in sampler:
+                mb_log_probs_old = log_probs_old[inds]
+                mb_states = states[inds]
+                mb_actions = actions[inds]
+                mb_returns = returns[inds]
+                loss_p, loss_v, loss_ent = agent.update(mb_log_probs_old, mb_states, mb_actions, mb_returns, mb_discounts)
+                loss_storage.append([loss_p, loss_v, loss_ent])
+                
+        total_rewards = np.sum(rewards, axis=0)
+        scores_window.append(np.mean(total_rewards)) # last 100 scores
+        mean_rewards.append(np.mean(total_rewards))  # get the average reward of the parallel environments
+
+    
+        if i_episode % 100 == 0:
+            print('\rEpisode {}\tAverage Score: {:.2f}'.format(i_episode, np.mean(scores_window)))
+            print(total_rewards)
+        if np.mean(scores_window)>=goal_score and np.mean(scores_window)>=best_score:            
+            torch.save(agent.policy.state_dict(), "policy_cartpole.pth")
+            best_score = np.mean(scores_window)
+    
+    return mean_rewards, loss_storage
+
+#train points
+mean_rewards, loss = train(400,'CartPole-v0')
+
+
+#Plot parameters and scores
+import matplotlib.pyplot as plt
+
+plt.rcParams['xtick.direction'] = 'in'
+plt.rcParams['ytick.direction'] = 'in'
+plt.rcParams['font.size'] = 10
+
+plt.plot(mean_rewards)
+plt.ylabel('Average score')
+plt.xlabel('episode')
+plt.show()
+
+#create animation
+from ppo import PPOAgent
+
+import gym
+
+env= gym.make('CartPole-v0')
+device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+agent = PPOAgent(state_size=env.observation_space.shape[0],
+                 action_size=env.action_space.n, 
+                 seed=0,
+                 hidden_layers=[64,64],
+                 lr_policy=1e-4, 
+                 use_reset=True,
+                 device=device)
+
+agent.policy.load_state_dict(torch.load('policy_cartpole.pth', map_location=lambda storage, loc: storage))
+
+from matplotlib import animation, rc
+rc('animation', html='jshtml')
+
+
+# function to animate a list of frames
+def animate_frames(frames):
+    plt.axis('off')
+
+    # color option for plotting
+    # use Greys for greyscale
+    cmap = None if len(frames[0].shape)==3 else 'Greys'
+    patch = plt.imshow(frames[0], cmap=cmap)  
+
+    fanim = animation.FuncAnimation(plt.gcf(), \
+        lambda x: patch.set_data(frames[x]), frames = len(frames), interval=50)
+    
+    return fanim
+
+def play(env, policy, time):
+    frame1 = env.reset()
+    
+    anim_frames = []
+    
+    for i in range(time):
+        
+        anim_frames.append(env.render(mode='rgb_array'))
+        frame_input = torch.from_numpy(frame1).unsqueeze(0).float().to(device)
+        action = policy.act(frame_input)['a'].cpu().numpy()
+        frame1, _, is_done, _ = env.step(int(action))
+
+        if is_done:
+            print("reward :", i+1)
+            break
+    
+    env.close()
+    
+    return animate_frames(anim_frames)
+
+
+play(env, agent.policy, 200)
+
---
+++ vtrace.py
@@ -0,0 +1,94 @@
+import numpy as np 
+import tensorflow as tf
+import tensorflow_probability as tfp
+import trfl
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+
+from model import FCNet, ConvNet, Policy
+
+class VtraceAgent:
+    def __init__(self, 
+                 state_size,
+                 action_size, 
+                 seed,
+                 hidden_layers,
+                 lr_policy, 
+                 use_reset, 
+                 device,
+                 discount=0.99,
+                 entropy_cost=0.,
+                 baseline_cost = 1.,         
+                ):
+        
+        #Setting hyperparameters
+        self.lr_policy = lr_policy
+        self.discount = discount
+        self.entropy_cost = entropy_cost
+        self.baseline_cost = baseline_cost
+        
+
+        #self.main_net = ConvNet(state_size, feature_dim, seed, use_reset, input_channel).to(device)
+        self.main_net = FCNet(state_size, seed, hidden_layers=[64,64], use_reset=True, act_fnc=F.relu).to(device)
+        self.policy = Policy(state_size, action_size, seed, self.main_net).to(device)
+        self.optimizer = optim.Adam(self.policy.parameters(), lr=lr_policy)
+        self.device = device
+        
+    
+
+    def update(self, log_prob_old, states, actions, rewards, discounts):
+    
+        #traj_info = self.policy.act(states, actions)
+        
+        tfd = tfp.distributions
+        states= states[:-1]
+        actions = actions[:-1]  # [T-1]
+        rewards = rewards[:-1]  # [T-1]
+        discounts = discounts[:-1]  # [T-1]
+        
+        # Compute importance sampling weights: current policy / behavior policy.
+        #behaviour_logits arethe logits from the actor network 
+        behaviour_logits,logits = self.policy(states)
+        values = nn.softmax(dim=1)(logits)
+        #logits are the logits from current policy network
+        pi_behaviour = tfd.Categorical(logits=behaviour_logits)
+        pi_target = tfd.Categorical(logits=logits)
+        log_rhos = pi_target.log_prob(actions) - pi_behaviour.log_prob(actions)
+         
+
+        # Critic loss.
+        #vs are the vtrace targets 
+        vtrace_returns = trfl.vtrace_from_importance_weights(
+          log_rhos=tf.cast(log_rhos, tf.float32),
+          discounts=tf.cast(self.discount * discounts, tf.float32),
+          rewards=tf.cast(rewards, tf.float32),
+          values=tf.cast(values[:-1], tf.float32),
+          bootstrap_value=values[-1],
+          )
+        #values are softmax values of current policy
+        critic_loss = tf.square(vtrace_returns.vs - values)
+
+        # Policy-gradient loss.
+        policy_gradient_loss = trfl.policy_gradient(
+          policies=pi_target,
+          actions=actions,
+          action_values=vtrace_returns.pg_advantages,
+           )
+
+        # Entropy regulariser.
+        entropy_loss = trfl.policy_entropy_loss(pi_target).loss
+
+
+        # Combine weighted sum of actor & critic losses.
+        loss = tf.reduce_mean(policy_gradient_loss +
+                            self._baseline_cost * critic_loss +
+                            self._entropy_cost * entropy_loss)
+        
+        self.optimizer.zero_grad()
+        (loss).backward()
+        #nn.utils.clip_grad_norm_(self.policy.parameters(), 5)
+        self.optimizer.step()
+
+        return policy_gradient_loss.data.cpu().numpy(), critic_loss.data.cpu().numpy(), entropy_loss.data.cpu().numpy()
